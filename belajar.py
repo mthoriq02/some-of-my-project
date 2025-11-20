99e0a6204a5c0f5f277e5aafed44e7b7f8f2d3b9
mthoriq02 <muhammadthoriq341@gmail.com>
2025-11-20 20:38:00 +0700

    Add initial implementation of various Python programs including a simple calculator, data structures, and user input handling

--- belajar.py
+++ belajar.py
@@ -8,7 +8,9 @@ class Kalkulator:
         
         self.after_equal = False
         self.screen = StringVar()
-        self.screen_entry = CTkEntry(master, textvariable=self.screen, width=190, height=40, font=('Arial', 11))
+        self.screen_entry = CTkEntry(
+            master, textvariable=self.screen, width=190,height=40, font=('Arial', 11)
+            )
         self.screen_entry.grid(row=0, column=0, columnspan=25, padx=10, pady=10)
 
         self.every_button = [
@@ -21,16 +23,32 @@ class Kalkulator:
 
         for but, rows, col in self.every_button:
             if but == 'C':
-                CTkButton(master, text=but, width=40, height=40, command=self.clear).grid(row=rows, column=col, padx=(9, 5), pady=(5, 5))
+                CTkButton(
+                    master, text=but, width=40, height=40, command=self.clear
+                    ).grid(
+                        row=rows, column=col, padx=(9, 5), pady=(5, 5)
+                        )
 
             elif but =='<-':
-                CTkButton(master, text=but, width=40, height=40, command=self.remove).grid(row=rows, column=col, padx=(9, 5), pady=(5, 5))
+                CTkButton(
+                    master, text=but, width=40, height=40, command=self.remove
+                    ).grid(
+                        row=rows, column=col, padx=(9, 5), pady=(5, 5)
+                        )
 
             elif but == '=':
-                CTkButton(master, text=but, width=40, height=40, command=self.sama_dengan).grid(row=rows, column=col, padx=(9, 5), pady=(5, 5))
+                CTkButton(
+                    master, text=but, width=40, height=40, command=self.sama_dengan
+                    ).grid(
+                        row=rows, column=col, padx=(9, 5), pady=(5, 5)
+                        )
 
             else:
-                CTkButton(master, text=but, width=40, height=40, command=lambda t=but:self.press(t)).grid(row=rows, column=col, padx=(9, 5), pady=(5, 5))
+                CTkButton(
+                    master, text=but, width=40, height=40, command=lambda t=but:self.press(t)
+                    ).grid(
+                        row=rows, column=col, padx=(9, 5), pady=(5, 5)
+                        )
 
     def press(self, values):
         if self.after_equal:
